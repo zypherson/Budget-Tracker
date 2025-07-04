73eb038518963e62307c9cb9764e237549b8d077
zypherson <taylorzypherson@gmail.com>
2025-07-04 19:47:15 -0400

    updated app js to render form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import Header from './Components/Header'
+import { useState } from 'react';
+import Header from '../components/Header';
+import TransactionForm from '../components/TransactionForm';
 
 function App() {
-  return(
+  const [transactions, setTransactions] = useState([]);
+
+  const handleAdd = (newTransaction) => {
+    setTransactions([newTransaction, ...transactions]);
+  };
+
+  return (
     <div>
       <Header />
+      <TransactionForm onAdd={handleAdd} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
