1300528264fa5c907085f5b730e379a489155315
zypherson <taylorzypherson@gmail.com>
2025-07-10 01:55:08 -0400

    implemented deletion logic in app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Header from '../components/Header';
 import TransactionForm from '../components/TransactionForm';
+import TransactionList from '../components/TransactionList';
 
 function App() {
   const [transactions, setTransactions] = useState([]);
@@ -8,12 +9,21 @@ function App() {
   const handleAdd = (newTransaction) => {
     setTransactions([newTransaction, ...transactions]);
   };
+  const handleDelete = (idToDelete) => {
+    setTransactions(transactions.filter((t) => t.id !== idToDelete));
+  };
+  
 
   return (
     <div>
       <Header />
       <TransactionForm onAdd={handleAdd} />
+      <TransactionList
+  transactions={transactions}
+  onDelete={handleDelete}
+/>
     </div>
+    
   );
 }
 
